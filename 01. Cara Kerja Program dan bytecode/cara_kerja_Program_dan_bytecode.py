025c3306143d4a3148394d85a94186e52de3694b
rasuantaputra <rasuantaportofolio@gmail.com>
2021-09-01 14:40:21 +0700

    menambang baris program print agar dapat terlihat perbeaaan waktunya

--- 01. Cara Kerja Program dan bytecode/cara_kerja_Program_dan_bytecode.py
+++ 01. Cara Kerja Program dan bytecode/cara_kerja_Program_dan_bytecode.py
@@ -4,6 +4,16 @@ startTime = time.time()
 
 print('halo')
 
+print('1')
+print('2')
+print('3')
+print('4')
+print('5')
+print('6')
+print('7')
+print('8')
+print('9')
+
 """
 command untuk mengubah ke bytecode
 windows :
