e8e014d7c80e6e1db76c1770a63e7cce82becb70
rasuantaputra <rasuantaportofolio@gmail.com>
2021-09-11 09:37:26 +0700

    membuat program komparasi assigment object

--- 10. Operasi Kmparasi/operasi_komparasi.py
+++ 10. Operasi Kmparasi/operasi_komparasi.py
@@ -17,8 +17,22 @@ yang akan dipelajari di sini adalah komparasi untuk assigment object
 
 python 3 kebawah bisa menggunakan "is" dan "is not" untuk literal.
 Tapi di python 3 udah gabisa
+
+biasanya nanti akan digunakan saat menggunakan OOP
 """
 
+x = 5
+y = 5
+
+# jika nanti di run, x dan y akan diletakan pada memory yang sama
+# hal tersebut dikarenakan nilai x dan y sama (dari tipe data maupun nilai)
+print('nilai x = ', x, ',id = ', hex(id(x)))
+print('nilai y = ', y, ',id = ', hex(id(y)))
+
+hasil =  x is y
+# hasil =  x is not y
+
+print('x is y = ', hasil) 
 
 
 
