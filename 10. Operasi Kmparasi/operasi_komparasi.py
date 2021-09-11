2ee4857e61e04ee93c2405874797d400fdbf3266
rasuantaputra <rasuantaportofolio@gmail.com>
2021-09-11 09:30:18 +0700

    Membuat deskripsi apa yang akan dipelajari

---
+++ 10. Operasi Kmparasi/operasi_komparasi.py
@@ -0,0 +1,40 @@
+"""
+Sama dengan dunia nyata operasi komparasi di python
+
+1. lebih dari (>)
+2. kurang dari (<)
+3. sama dengan (==)
+4. lebih dari sama dengan (>=)
+5. kurang dari sama dengan (<=)
+7. tidak sama dengan (!=)
+
+komparasi di sana untuk literal
+"""
+
+"""
+yang akan dipelajari di sini adalah komparasi untuk assigment object
+(is dan is not)
+
+python 3 kebawah bisa menggunakan "is" dan "is not" untuk literal.
+Tapi di python 3 udah gabisa
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
